3891839f8a8d1c5953b440f5d66c505d77bbca98
AM Infotrix <AM_Infotrix@Monalisa>
2016-08-04 12:37:08 +0530

    .

--- php/include/Config.php
+++ php/include/Config.php
@@ -5,20 +5,20 @@
  * Database configuration
  
  */
-// define ( 'DB_USERNAME', 'root' );
-// define ( 'DB_PASSWORD', '' );
-// define ( 'DB_HOST', 'localhost' );
-// define ( 'DB_NAME', 't4o' );
-// define ( 'DB_PORT', 3306 );
+define ( 'DB_USERNAME', 'root' );
+define ( 'DB_PASSWORD', '' );
+define ( 'DB_HOST', 'localhost' );
+define ( 'DB_NAME', 't4o' );
+define ( 'DB_PORT', 3306 );
 
 /**
  * for server
  */
-define ( 'DB_USERNAME', 'the4thofficial_ami1250000' );
-define ( 'DB_PASSWORD', 'pass@ami1250000' );
-define ( 'DB_HOST', 'localhost' );
-define ( 'DB_NAME', 'the4thofficial_matches' );
-define ( 'DB_PORT', 3306 );
+// define ( 'DB_USERNAME', 'the4thofficial_ami1250000' );
+// define ( 'DB_PASSWORD', 'pass@ami1250000' );
+// define ( 'DB_HOST', 'localhost' );
+// define ( 'DB_NAME', 'the4thofficial_matches' );
+// define ( 'DB_PORT', 3306 );
 
 define ( "api_token", "CEO6Xe2434mXUx81C9YDkEkdokzV0JHGJ6ZCfvoPSN1FFiB5cs7DSbaByTCy" );
 define ( "API_host", "https://api.soccerama.pro/v1.1/" );
